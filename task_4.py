4bb94b2c2a21ec1d060962bbff4e03483ad1f69a
nastya24704 <nastazeravina@gmail.com>
2025-04-28 00:49:43 +0700

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,1 +1,5 @@
-
+with open("input.txt", "r", encoding="utf-8") as f:
+    data = f.readlines()
+new_line = [line for line in data if len(data) >20]
+with open("output.txt", "w", encoding="utf-8") as f_2:
+    f_2.writelines(new_line)
