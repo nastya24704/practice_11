a8260027c722e3c8a7fdc7ca8ddebd18580ace87
nastya24704 <nastazeravina@gmail.com>
2025-04-28 09:24:58 +0700

    Update task_9.py

--- task_9.py
+++ task_9.py
@@ -1,1 +1,7 @@
+with open('input.txt', 'r', encoding="utf-8") as f:
+    data = f.readlines()
 
+with open('simple/output.txt', 'w', encoding="utf-8") as f_2:
+    for i in range(1, len(data) + 1):
+        if i % 2 == 0:
+            f_2.write(data[i - 1])
