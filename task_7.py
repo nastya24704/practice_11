312d66a1eebbe45af2e8f464956eacb47aec118f
nastya24704 <nastazeravina@gmail.com>
2025-04-28 07:34:37 +0700

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,1 +1,7 @@
+with open('input.txt', 'r') as f:
+    data = f.readlines()
 
+new_lines = [line for line in data if line.strip() != '100']
+
+with open('input.txt', 'w') as f:
+    f.writelines(new_lines)
